09f1d902d04d56e149be5fc02a70ce06a4f943ec
aziz_dhaifullah <aziz@investree.id>
2021-12-04 20:14:30 +0700

    [AZIZ] - error handling API

--- routes/api.php
+++ routes/api.php
@@ -14,19 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'register']);
 Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login']);
 
-Route::get('test', [App\Http\Controllers\Api\Test\ApiTestController::class, 'index'])->middleware('client');
+Route::middleware(['client'])->group(function () {
+    Route::get('test', [App\Http\Controllers\Api\Test\ApiTestController::class, 'index']);
 
-Route::prefix('asensing')->group(function () {
     Route::prefix('employee')->group(function () {
-        Route::get('/', [App\Http\Controllers\Api\Admin\EmployeeApiController::class, 'getEmployeeList'])->middleware('client');
-        Route::post('/insert', [App\Http\Controllers\Api\Admin\EmployeeApiController::class, 'createBulkEmployee'])->middleware('client');
+        Route::get('/', [App\Http\Controllers\Api\Admin\EmployeeApiController::class, 'getEmployeeList']);
+        Route::post('/insert', [App\Http\Controllers\Api\Admin\EmployeeApiController::class, 'createBulkEmployee']);
     });
 });
+
+
 
